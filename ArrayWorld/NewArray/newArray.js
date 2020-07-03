8ff0902c391c5a9001a67d3523bbb5531b53704a
mmkvdev <madhuv299@gmail.com>
2020-07-03 21:02:14 +0530

    Update: New Array

---
+++ ArrayWorld/NewArray/newArray.js
@@ -0,0 +1,7 @@
+// newArray is just an another approach for playing around with JS Arrays
+
+// creating an array
+let brands = new Array(2); // what does it do? => basically creates an empty array with n items but with length n
+console.log(brands);
+console.log(brands[0]); // fuck you!!
+console.log(brands.length); // screw it!!!
