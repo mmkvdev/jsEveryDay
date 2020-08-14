1f3575c3c269c55998b9481b3ed5e43577e613e5
mmkvdev <madhuv299@gmail.com>
2020-08-14 21:41:56 +0530

    Update: String Slice

---
+++ Strings/slice.js
@@ -0,0 +1,7 @@
+// slice will extract parts of a string and returns the extracted parts of a string - takes start and end indexes to slice
+
+const slice = (str) => {
+    return str.slice(0,2);
+}
+
+console.log(slice("MMK VARMA COOL"))
